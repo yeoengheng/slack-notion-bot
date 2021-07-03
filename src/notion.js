a054c3b5a9cfdae5409f55b937ab73f7705a7dab
Eng Heng Yeo <enghengyeo12@hotmail.com>
2021-07-03 18:03:33 +0800

    export function

--- src/notion.js
+++ src/notion.js
@@ -1,10 +1,9 @@
 import { Client } from "@notionhq/client"
-console.log(process.env.NOTION_Key)
 const notion = new Client({ auth: process.env.NOTION_KEY })
 const databaseId = 'ca5141e1a6c7475daf88f36a4b6ad789'
 
 // Adding Item 
-async function addItem(text,content) {
+export async function addItem(text,content) {
   try {
     await notion.request({
       path: "pages",
@@ -41,9 +40,8 @@ async function addItem(text,content) {
       },
     })
     console.log("Success! Entry added.")
+    return true
   } catch (error) {
     console.error(error.body)
   }
 }
-
-addItem("Page Title","Content")
