b042f06167b05a17b057c9162ba6bf4220cd87bb
Eng Heng Yeo <enghengyeo12@hotmail.com>
2021-07-03 19:01:09 +0800

    Notion Logger

--- src/notion.js
+++ src/notion.js
@@ -1,5 +1,8 @@
-import { Client } from "@notionhq/client"
-const notion = new Client({ auth: process.env.NOTION_KEY })
+import { Client, LogLevel } from "@notionhq/client"
+const notion = new Client({ 
+  auth: process.env.NOTION_KEY,
+  logLevel: LogLevel.DEBUG,
+})
 const databaseId = 'ca5141e1a6c7475daf88f36a4b6ad789'
 
 // Adding Item 
