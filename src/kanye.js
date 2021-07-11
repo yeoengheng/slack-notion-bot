dbe2341acc83a071e966b8b910aa6991d87fb55d
Eng Heng Yeo <enghengyeo12@hotmail.com>
2021-07-11 22:57:40 +0800

    change to bread puns

--- src/kanye.js
+++ src/kanye.js
@@ -1,7 +1,7 @@
 import axios from 'axios' 
 
 export function getKanye(){
-    const promise = axios.get('https://api.kanye.rest/')
+    const promise = axios.get('https://my-bao-server.herokuapp.com/api/breadpuns')
     const dataPromise = promise.then(response => response.data.quote)
     return dataPromise
 }
